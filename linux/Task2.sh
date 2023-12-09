466747cb048a29f5a06ea9b70f92746e980b307e
savitha27 <151747483+savitha27@users.noreply.github.com>
2023-12-09 18:54:09 +0530

    Update Task2.sh from github

--- linux/Task2.sh
+++ linux/Task2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-myfile=$1
-read -p "Enter file name:" myfile
+myfile="hello world"
+read -p myfile
 echo "Number of lines in file is: `wc -l $myfile`"
 echo "Number of words in file is: `wc -w $myfile`"
 echo "Number of characters in file is: `wc -c $myfile`"
