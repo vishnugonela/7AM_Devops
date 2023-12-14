75e0cdb368f4dba260e64c8f1f7cfab23b4a8fd8
INDIRANI108 <151603172+INDIRANI108@users.noreply.github.com>
2023-12-14 19:01:19 +0530

    Update task1.groovy

--- jenkins/task1.groovy
+++ jenkins/task1.groovy
@@ -1,17 +1,36 @@
 pipeline {
-      agent any
-      stages {
-          stage('Master and slave details:') { 
-              steps {
-                 
-                 sh '''
+    agent any
+
+    stages {
+        stage('Master details') {
+            steps {
+                echo 'Running task on the master...'
+                // Add your master task commands here
+                
+                   sh '''
                  echo "Building the `hostname -I` the server hostname"
                  echo "the total memory of the space `free -m`"
                  echo "The total disk size in the node is `df -h`in "
                  
                  '''
+            }
+        }
+
+        stage('Slave Task') {
+            agent {
+                label 'Node1' // Replace with your slave label or name
+            }
+            steps {
+                echo 'Running task on the slave...'
+                // Add your slave task commands here
+                
+                   sh '''
+                 echo "Building the `hostname -I` the server hostname"
+                 echo "the total memory of the space `free -m`"
+                 echo "The total disk size in the node is `df -h`in "
                  
-              }
-          }
-      }
+                 '''
+            }
+        }
+    }
 }
