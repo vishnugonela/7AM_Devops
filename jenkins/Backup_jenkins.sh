00f31190beec9046a2cb246c4ad3a3712a1ac56e
pawannraj <98880956+pawannraj@users.noreply.github.com>
2024-08-08 10:55:05 +0530

    Update Backup_jenkins.sh

--- jenkins/Backup_jenkins.sh
+++ jenkins/Backup_jenkins.sh
@@ -2,8 +2,8 @@
 
 Backup_Dir="/home/ec2-user/backup"
 Jenkins_home="/var/lib/jenkins"
-
+echo "jenkins backup creating at $Backup_Dir"
 cp -a $Jenkins_home $Backup_Dir
 
-echo "jenkins backup created at $Backup_Dir"
+
 
