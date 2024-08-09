fdee4d8cf842ab8791c5bc11c07b1d1f13b10cbe
pawannraj <98880956+pawannraj@users.noreply.github.com>
2024-08-09 09:33:35 +0530

    Update Task1.sh

--- linux/Task1.sh
+++ linux/Task1.sh
@@ -2,16 +2,12 @@
 echo "Retrieving CPU and Memory Information:"
 cpuInfo=$(lscpu)
 memoryInfo=$(free -h)
-disk=$(df -h)
 systemInfo=$(uname -a)
                     echo "CPU Information:"
                     echo $cpuInfo
 
                     echo "Memory Information:"
                     echo $memoryInfo
-
-		    echo "disk usage"
-		    echo $disk
-
+		    
                     echo "System Information:"
                     echo $systemInfo
