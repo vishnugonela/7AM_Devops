0686845b0114a9f72836ea60d0d7505f2a1c35ce
Sunit-Nayak <131778342+Sunit-Nayak@users.noreply.github.com>
2024-09-08 00:22:24 +0530

    Create task7.sh

---
+++ linux/task7.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Write a shell script to accept input from user as their username only.must be"string" "UPPERCASE" "should not contain spaces"
+#
+echo "enter username :"
+read username
+echo "enter password :"
+read password
