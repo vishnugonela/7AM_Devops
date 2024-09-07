214b824216e7a9ee614b8a1d491aeaa059c74a80
Sunit-Nayak <131778342+Sunit-Nayak@users.noreply.github.com>
2024-09-08 00:18:49 +0530

    Update Tsk5.sh

--- linux/Tsk5.sh
+++ linux/Tsk5.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#shell script to install a package called 'tomcat'
+#Before installing check whether package installed or not
 
-package_name="tomcat"
- sudo apt install -y $package_name
+
+package="tomcat"
+
+if [ dnf list installed "$package" ]
+then
+    echo "$package is installed."
+else
+
+    echo "install $package"
+
+
+    apt install -y $package
+
